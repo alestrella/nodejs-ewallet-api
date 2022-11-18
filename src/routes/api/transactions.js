26a3cb7e5c6b2d233df327d22b15a50a5ff2b089
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-18 15:22:12 +0200

    transactions paths mock controller added

---
+++ src/routes/api/transactions.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+// const ctrlTransactions = require("../../controllers/transactions");
+const ctrlWrapper = require("../../helpers/controlWrapper");
+// const validate = require("../../middleware/validate");
+const isLoggedIn = require("../../middlewares");
+const { mockFunction } = require("../../controllers/mockController");
+
+router.get("/", isLoggedIn, ctrlWrapper(ctrlTransactions.getAll));
+
+router.get("/:pageNumber", isLoggedIn, ctrlWrapper(mockFunction));
+
+router.post(
+  "/",
+  isLoggedIn,
+  //   validate(addTransactionSchema),
+  ctrlWrapper((req, res) => {
+    res.status(200).json({ message: "post done" });
+  })
+);
+
+router.delete(
+  "/:transactionId",
+  isLoggedIn,
+  ctrlWrapper((req, res) => {
+    res.status(200).json({ message: "delete done" });
+  })
+);
+
+module.exports = router;
