cd5beb5a0589a7a35713414168d32bccbb2f9fa2
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-18 15:37:06 +0200

    transactions routes mocked

--- src/routes/api/transactions.js
+++ src/routes/api/transactions.js
@@ -2,18 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 // const ctrlTransactions = require("../../controllers/transactions");
-const ctrlWrapper = require("../../helpers/controlWrapper");
+const ctrlWrapper = require("../../helpers/ctrlWrapper");
 // const validate = require("../../middleware/validate");
-const isLoggedIn = require("../../middlewares");
+// const isLoggedIn = require("../../middlewares");
 const { mockFunction } = require("../../controllers/mockController");
 
-router.get("/", isLoggedIn, ctrlWrapper(ctrlTransactions.getAll));
+router.get(
+  "/",
+  // isLoggedIn,
+  ctrlWrapper(mockFunction)
+);
 
-router.get("/:pageNumber", isLoggedIn, ctrlWrapper(mockFunction));
+router.get(
+  "/:pageNumber",
+  // isLoggedIn,
+  ctrlWrapper(mockFunction)
+);
 
 router.post(
   "/",
-  isLoggedIn,
+  //   isLoggedIn,
   //   validate(addTransactionSchema),
   ctrlWrapper((req, res) => {
     res.status(200).json({ message: "post done" });
@@ -22,7 +30,7 @@ router.post(
 
 router.delete(
   "/:transactionId",
-  isLoggedIn,
+  //   isLoggedIn,
   ctrlWrapper((req, res) => {
     res.status(200).json({ message: "delete done" });
   })
