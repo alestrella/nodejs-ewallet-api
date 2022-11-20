4762be740a8afa0cc9df6d7f7a13137706de8eb8
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-20 21:21:31 +0200

    recalculation function done

--- src/controllers/transactions/recalculateBalance.js
+++ src/controllers/transactions/recalculateBalance.js
@@ -3,16 +3,38 @@ const { Transaction } = require("../../models/transaction");
 const recalculateBalance = async (owner, date, correction) => {
   // recalculate collection after the data
   console.log("recalculation function running from ", date);
-  const result = await Transaction.updateMany(
-    {
-      $match: {
-        owner,
-        _createdAt: { $gt: date },
+  try {
+    const result = await Transaction.aggregate([
+      {
+        $match: {
+          owner,
+          createdAt: { $gt: date },
+        },
       },
-    },
-    { $set: { balance: { $add: { balance, correction } } } }
-  );
-  return result;
+      {
+        $project: {
+          _id: "$_id",
+          category: "$category",
+          income: "$income",
+          comment: "$comment",
+          sum: "$sum",
+          balance: { $sum: ["$balance", correction] },
+          createdAt: "$createdAt",
+        },
+      },
+      {
+        $merge: {
+          into: "transactions",
+          on: "_id",
+          whenMatched: "merge",
+          whenNotMatched: "insert",
+        },
+      },
+    ]);
+    return 1;
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = recalculateBalance;
