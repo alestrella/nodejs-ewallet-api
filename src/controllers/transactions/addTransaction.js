34e34e30a6050d0ddcb7603c8c24a37003fdac3d
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-20 23:26:24 +0200

    fix: transaction validation on add edited

--- src/controllers/transactions/addTransaction.js
+++ src/controllers/transactions/addTransaction.js
@@ -7,7 +7,12 @@ const addTransaction = async (req, res) => {
   let balance = await getBalance(owner);
   balance = body.income ? balance + body.sum : balance - body.sum;
   const { _id, income, comment, category, sum, createdAt } =
-    await Transaction.create({ ...body, owner, balance });
+    await Transaction.create({
+      ...body,
+      category: body.category || "000000000000000000000000",
+      owner,
+      balance,
+    });
   const resData = {
     id: _id,
     date: createdAt,
