9e0d95484806728585d6c862c3eef05a38cd6835
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-20 15:26:23 +0200

    recalculate function added (not working)

--- src/controllers/transactions/recalculateBalance.js
+++ src/controllers/transactions/recalculateBalance.js
@@ -1,6 +1,18 @@
-const recalculateBalance = async (date) => {
+const { Transaction } = require("../../models/transaction");
+
+const recalculateBalance = async (owner, date, correction) => {
   // recalculate collection after the data
   console.log("recalculation function running from ", date);
+  const result = await Transaction.updateMany(
+    {
+      $match: {
+        owner,
+        _createdAt: { $gt: date },
+      },
+    },
+    { $set: { balance: { $add: { balance, correction } } } }
+  );
+  return result;
 };
 
 module.exports = recalculateBalance;
