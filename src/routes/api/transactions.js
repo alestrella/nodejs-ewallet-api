5f673bdc7bc2d67e7940bbc477f62357685a623c
Alexandra Sabatin <s4batin@gmail.com>
2022-11-20 10:59:57 +0200

    add auth function to transaction routes

--- src/routes/api/transactions.js
+++ src/routes/api/transactions.js
@@ -3,44 +3,43 @@ const router = express.Router();
 
 const ctrlTransactions = require("../../controllers/transactions");
 const ctrlWrapper = require("../../helpers/ctrlWrapper");
-const { validator } = require("../../middlewares");
-// const isLoggedIn = require("../../middlewares");
+const { validator, isLoggedIn } = require("../../middlewares");
 const { transactionJoiSchemas } = require("../../models/transaction");
 
 router.get(
   "/",
-  // isLoggedIn,
+  isLoggedIn,
   //fake log func
-  (req, res, next) => {
-    req.user = {
-      _id: "123456789012345678901234",
-      username: "tester",
-      email: "email@email.xx",
-    };
-    next();
-  },
+  // (req, res, next) => {
+  //   req.user = {
+  //     _id: "123456789012345678901234",
+  //     username: "tester",
+  //     email: "email@email.xx",
+  //   };
+  //   next();
+  // },
   ctrlWrapper(ctrlTransactions.getTransactions)
 );
 
 router.post(
   "/",
-  //   isLoggedIn,
+  isLoggedIn,
   validator(transactionJoiSchemas.addTransactionSchema),
   //fake log func
-  (req, res, next) => {
-    req.user = {
-      _id: "123456789012345678901234",
-      username: "tester",
-      email: "email@email.xx",
-    };
-    next();
-  },
+  // (req, res, next) => {
+  //   req.user = {
+  //     _id: "123456789012345678901234",
+  //     username: "tester",
+  //     email: "email@email.xx",
+  //   };
+  //   next();
+  // },
   ctrlWrapper(ctrlTransactions.addTransaction)
 );
 
 router.delete(
   "/:transactionId",
-  //   isLoggedIn,
+  isLoggedIn,
   ctrlWrapper(ctrlTransactions.deleteTransaction)
 );
 
