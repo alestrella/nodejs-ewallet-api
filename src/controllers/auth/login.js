c3b03f6c5f524ac0533eb83a76fb06f4a2786746
Alexandra Sabatin <s4batin@gmail.com>
2022-11-21 12:46:51 +0200

    remove clg

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -12,7 +12,6 @@ const login = async (req, res) => {
 
   const user = await User.findOne({ email });
   const passwordCompare = await bcrypt.compare(password, user.password);
-  console.log(passwordCompare);
 
   if (!user || !passwordCompare) {
     throw requestError(401, "Email or password is wrong");
