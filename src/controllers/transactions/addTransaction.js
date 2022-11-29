8b4539134de930057f0da0ed8680813540496484
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-29 23:04:41 +0200

    add recalculates transactions and balance

--- src/controllers/transactions/addTransaction.js
+++ src/controllers/transactions/addTransaction.js
@@ -1,28 +1,32 @@
 const { date } = require("joi");
 const { Transaction } = require("../../models/transaction");
 const getBalance = require("./getBalance");
+const recalculateBalance = require("./recalculateBalance");
 
 const addTransaction = async (req, res) => {
   const { _id: owner } = req.user;
   const body = req.body;
-  let balance = await getBalance(owner);
+  const date = body.date || new Date().toISOString();
+  let balance = await getBalance(owner, date);
   balance = body.income ? balance + body.sum : balance - body.sum;
-  const { _id, income, comment, category, sum, createdAt, operationDate } =
-    await Transaction.create({
-      ...body,
-      category: body.category || "000000000000000000000000",
-      owner,
-      balance,
-    });
+  const transaction = await Transaction.create({
+    ...body,
+    category: body.category || "000000000000000000000000",
+    owner,
+    balance,
+    operationDate: date,
+  });
+  const correction = transaction.income ? transaction.sum : -transaction.sum;
+  await recalculateBalance(owner, transaction.operationDate, correction);
+  const { _id, income, comment, category, sum, operationDate } = transaction;
   const resData = {
     id: _id,
-    date: createdAt,
+    date: operationDate,
     income,
     comment,
     category,
     sum,
     balance,
-    operationDate,
   };
   res.status(201).json(resData);
 };
