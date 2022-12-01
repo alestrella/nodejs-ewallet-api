a4f39665a3ca05353082334e62bcb07fbc0ee973
Alexandra Sabatin <s4batin@gmail.com>
2022-12-01 12:16:52 +0200

    fix sign up

--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -1,24 +1,27 @@
 const bcrypt = require("bcryptjs");
 const { User } = require("../../models/user");
-const { requestError, sendEmail } = require("../../helpers");
+const { requestError } = require("../../helpers");
 const { updateTokens } = require("../../services/updateTokens");
+const sendEmail = require("../../helpers/sendEmail");
 
 const signup = async (req, res) => {
   const { email, password, username } = req.body;
 
-  const user = await User.findOne({ email });
-  if (user) {
+  const oldUser = await User.findOne({ email });
+  if (oldUser) {
     throw requestError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({
+  const user = await User.create({
     email,
     password: hashPassword,
     username,
   });
-  const userId = newUser._id;
+
+  const userId = user._id;
   const tokens = await updateTokens(userId);
+
   await sendEmail(user);
 
   res.status(201).json({
